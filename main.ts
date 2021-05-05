92ed089c0e19c77fc6b9f6e67fb28aa27c9c32c2
Emily Pang <82404497+pingpongracketpencilcase@users.noreply.github.com>
2021-05-05 13:54:23 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,31 @@
+controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
+    mySprite.vy = -100
+})
+function level1 () {
+    tiles.setTilemap(tilemap`level1`)
+}
+let mySprite: Sprite = null
+mySprite = sprites.create(img`
+    . . . . . f f 4 4 f f . . . . . 
+    . . . . f 5 4 5 5 4 5 f . . . . 
+    . . . f e 4 5 5 5 5 4 e f . . . 
+    . . f b 3 e 4 4 4 4 e 3 b f . . 
+    . . f 3 3 3 3 3 3 3 3 3 3 f . . 
+    . f 3 3 e b 3 e e 3 b e 3 3 f . 
+    . f 3 3 f f e e e e f f 3 3 f . 
+    . f b b f b f e e f b f b b f . 
+    . f b b e 1 f 4 4 f 1 e b b f . 
+    f f b b f 4 4 4 4 4 4 f b b f f 
+    f b b f f f e e e e f f f b b f 
+    . f e e f b d d d d b f e e f . 
+    . . e 4 c d d d d d d c 4 e . . 
+    . . e f b d b d b d b b f e . . 
+    . . . f f 1 d 1 d 1 d f f . . . 
+    . . . . . f f b b f f . . . . . 
+    `, SpriteKind.Player)
+mySprite.ay = 200
+level1()
+forever(function () {
+    controller.moveSprite(mySprite, 100, 0)
+    scene.cameraFollowSprite(mySprite)
+})
