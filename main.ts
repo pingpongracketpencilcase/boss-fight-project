a63fb7a54ccb9969e146f44b15724f7ccb54083b
Emily Pang <82404497+pingpongracketpencilcase@users.noreply.github.com>
2021-05-07 13:56:22 -0400

    Update main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,38 +1,242 @@
 namespace SpriteKind {
     export const bird = SpriteKind.create()
+    export const background = SpriteKind.create()
+    export const star = SpriteKind.create()
+    export const star2 = SpriteKind.create()
+    export const star3 = SpriteKind.create()
+    export const star4 = SpriteKind.create()
+    export const star5 = SpriteKind.create()
+    export const star6 = SpriteKind.create()
+    export const star7 = SpriteKind.create()
 }
+sprites.onOverlap(SpriteKind.star5, SpriteKind.Player, function (sprite, otherSprite) {
+    info.changeScoreBy(1)
+    star5.destroy()
+})
+sprites.onOverlap(SpriteKind.bird, SpriteKind.background, function (sprite, otherSprite) {
+    star.destroy()
+})
 controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
     mySprite.vy = -100
 })
 function level1 () {
     tiles.setTilemap(tilemap`level1`)
-    tiles.placeOnTile(mySprite, tiles.getTileLocation(8, 88))
-    bird.setImage(img`
-        ....ffffff.........ccc..
-        ....ff22ccf.......cc4f..
-        .....ffccccfff...cc44f..
-        ....cc24442222cccc442f..
-        ...c9b4422222222cc422f..
-        ..c999b2222222222222fc..
-        .c2b99111b222222222c22c.
-        c222b111992222ccccccc22f
-        f222222222222c222ccfffff
-        .f2222222222442222f.....
-        ..ff2222222cf442222f....
-        ....ffffffffff442222c...
-        .........f2cfffc2222c...
-        .........fcc2ffffffff...
-        ..........fc2ffff.......
-        ...........fffff........
+    tiles.placeOnTile(mySprite, tiles.getTileLocation(4, 88))
+    star.setImage(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . b d b . . . . . . 
+        . . . . . . . b d b c . . . . . 
+        . . . . b b c 5 5 5 c b b . . . 
+        . . . . b 5 5 5 1 5 5 5 b . . . 
+        . . . c c 5 5 5 1 5 5 5 c c . . 
+        . . b b 5 5 5 1 1 1 5 5 5 b b . 
+        . . d d 5 1 1 1 1 1 1 1 5 d d . 
+        . . b b 5 5 5 1 1 1 5 5 5 b b . 
+        . . . c c 5 5 5 1 5 5 5 c c . . 
+        . . . . b 5 5 5 1 5 5 5 b . . . 
+        . . . . b b c 5 5 5 c b b . . . 
+        . . . . . . c b d b c . . . . . 
+        . . . . . . . b d b . . . . . . 
+        . . . . . . . . . . . . . . . . 
         `)
-    tiles.placeOnTile(bird, tiles.getTileLocation(15, 85))
-    bird.setVelocity(-100, 0)
+    star2.setImage(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . b d b . . . . . . 
+        . . . . . . . b d b c . . . . . 
+        . . . . b b c 5 5 5 c b b . . . 
+        . . . . b 5 5 5 1 5 5 5 b . . . 
+        . . . c c 5 5 5 1 5 5 5 c c . . 
+        . . b b 5 5 5 1 1 1 5 5 5 b b . 
+        . . d d 5 1 1 1 1 1 1 1 5 d d . 
+        . . b b 5 5 5 1 1 1 5 5 5 b b . 
+        . . . c c 5 5 5 1 5 5 5 c c . . 
+        . . . . b 5 5 5 1 5 5 5 b . . . 
+        . . . . b b c 5 5 5 c b b . . . 
+        . . . . . . c b d b c . . . . . 
+        . . . . . . . b d b . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `)
+    star3.setImage(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . b d b . . . . . . 
+        . . . . . . . b d b c . . . . . 
+        . . . . b b c 5 5 5 c b b . . . 
+        . . . . b 5 5 5 1 5 5 5 b . . . 
+        . . . c c 5 5 5 1 5 5 5 c c . . 
+        . . b b 5 5 5 1 1 1 5 5 5 b b . 
+        . . d d 5 1 1 1 1 1 1 1 5 d d . 
+        . . b b 5 5 5 1 1 1 5 5 5 b b . 
+        . . . c c 5 5 5 1 5 5 5 c c . . 
+        . . . . b 5 5 5 1 5 5 5 b . . . 
+        . . . . b b c 5 5 5 c b b . . . 
+        . . . . . . c b d b c . . . . . 
+        . . . . . . . b d b . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `)
+    star4.setImage(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . b d b . . . . . . 
+        . . . . . . . b d b c . . . . . 
+        . . . . b b c 5 5 5 c b b . . . 
+        . . . . b 5 5 5 1 5 5 5 b . . . 
+        . . . c c 5 5 5 1 5 5 5 c c . . 
+        . . b b 5 5 5 1 1 1 5 5 5 b b . 
+        . . d d 5 1 1 1 1 1 1 1 5 d d . 
+        . . b b 5 5 5 1 1 1 5 5 5 b b . 
+        . . . c c 5 5 5 1 5 5 5 c c . . 
+        . . . . b 5 5 5 1 5 5 5 b . . . 
+        . . . . b b c 5 5 5 c b b . . . 
+        . . . . . . c b d b c . . . . . 
+        . . . . . . . b d b . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `)
+    star5.setImage(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . b d b . . . . . . 
+        . . . . . . . b d b c . . . . . 
+        . . . . b b c 5 5 5 c b b . . . 
+        . . . . b 5 5 5 1 5 5 5 b . . . 
+        . . . c c 5 5 5 1 5 5 5 c c . . 
+        . . b b 5 5 5 1 1 1 5 5 5 b b . 
+        . . d d 5 1 1 1 1 1 1 1 5 d d . 
+        . . b b 5 5 5 1 1 1 5 5 5 b b . 
+        . . . c c 5 5 5 1 5 5 5 c c . . 
+        . . . . b 5 5 5 1 5 5 5 b . . . 
+        . . . . b b c 5 5 5 c b b . . . 
+        . . . . . . c b d b c . . . . . 
+        . . . . . . . b d b . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `)
+    star6.setImage(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . b d b . . . . . . 
+        . . . . . . . b d b c . . . . . 
+        . . . . b b c 5 5 5 c b b . . . 
+        . . . . b 5 5 5 1 5 5 5 b . . . 
+        . . . c c 5 5 5 1 5 5 5 c c . . 
+        . . b b 5 5 5 1 1 1 5 5 5 b b . 
+        . . d d 5 1 1 1 1 1 1 1 5 d d . 
+        . . b b 5 5 5 1 1 1 5 5 5 b b . 
+        . . . c c 5 5 5 1 5 5 5 c c . . 
+        . . . . b 5 5 5 1 5 5 5 b . . . 
+        . . . . b b c 5 5 5 c b b . . . 
+        . . . . . . c b d b c . . . . . 
+        . . . . . . . b d b . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `)
+    star7.setImage(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . b d b . . . . . . 
+        . . . . . . . b d b c . . . . . 
+        . . . . b b c 5 5 5 c b b . . . 
+        . . . . b 5 5 5 1 5 5 5 b . . . 
+        . . . c c 5 5 5 1 5 5 5 c c . . 
+        . . b b 5 5 5 1 1 1 5 5 5 b b . 
+        . . d d 5 1 1 1 1 1 1 1 5 d d . 
+        . . b b 5 5 5 1 1 1 5 5 5 b b . 
+        . . . c c 5 5 5 1 5 5 5 c c . . 
+        . . . . b 5 5 5 1 5 5 5 b . . . 
+        . . . . b b c 5 5 5 c b b . . . 
+        . . . . . . c b d b c . . . . . 
+        . . . . . . . b d b . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `)
+    tiles.placeOnTile(star, tiles.getTileLocation(randint(0, 10), randint(0, 80)))
+    tiles.placeOnTile(star2, tiles.getTileLocation(randint(0, 10), randint(0, 80)))
+    tiles.placeOnTile(star3, tiles.getTileLocation(randint(0, 10), randint(0, 80)))
+    tiles.placeOnTile(star4, tiles.getTileLocation(randint(0, 10), randint(0, 80)))
+    tiles.placeOnTile(star5, tiles.getTileLocation(randint(0, 10), randint(0, 80)))
+    tiles.placeOnTile(star6, tiles.getTileLocation(randint(0, 10), randint(0, 80)))
+    tiles.placeOnTile(star7, tiles.getTileLocation(randint(0, 10), randint(0, 80)))
+    star.setVelocity(0, randint(30, 80))
+    star2.setVelocity(0, randint(30, 80))
+    star3.setVelocity(0, randint(30, 80))
+    star4.setVelocity(0, randint(30, 80))
+    star5.setVelocity(0, randint(30, 80))
+    star6.setVelocity(0, randint(30, 80))
+    star7.setVelocity(0, randint(30, 80))
 }
-scene.onOverlapTile(SpriteKind.Player, assets.tile`transparency16`, function (sprite, location) {
-	
+scene.onOverlapTile(SpriteKind.star2, sprites.dungeon.hazardLava0, function (sprite, location) {
+    star2.destroy()
 })
-let bird: Sprite = null
+scene.onOverlapTile(SpriteKind.star4, sprites.dungeon.hazardLava0, function (sprite, location) {
+    star4.destroy()
+})
+sprites.onOverlap(SpriteKind.star2, SpriteKind.Player, function (sprite, otherSprite) {
+    info.changeScoreBy(1)
+    star2.destroy()
+})
+sprites.onOverlap(SpriteKind.star3, SpriteKind.Player, function (sprite, otherSprite) {
+    info.changeScoreBy(1)
+    star3.destroy()
+})
+scene.onOverlapTile(SpriteKind.star, sprites.dungeon.hazardLava0, function (sprite, location) {
+    star.destroy()
+})
+sprites.onOverlap(SpriteKind.star6, SpriteKind.Player, function (sprite, otherSprite) {
+    info.changeScoreBy(1)
+    star6.destroy()
+})
+sprites.onOverlap(SpriteKind.star7, SpriteKind.Player, function (sprite, otherSprite) {
+    info.changeScoreBy(1)
+    star7.destroy()
+})
+scene.onOverlapTile(SpriteKind.star5, sprites.dungeon.hazardLava0, function (sprite, location) {
+    star5.destroy()
+})
+sprites.onOverlap(SpriteKind.star, SpriteKind.Player, function (sprite, otherSprite) {
+    info.changeScoreBy(1)
+    star.destroy()
+    star.setImage(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . b d b . . . . . . 
+        . . . . . . . b d b c . . . . . 
+        . . . . b b c 5 5 5 c b b . . . 
+        . . . . b 5 5 5 1 5 5 5 b . . . 
+        . . . c c 5 5 5 1 5 5 5 c c . . 
+        . . b b 5 5 5 1 1 1 5 5 5 b b . 
+        . . d d 5 1 1 1 1 1 1 1 5 d d . 
+        . . b b 5 5 5 1 1 1 5 5 5 b b . 
+        . . . c c 5 5 5 1 5 5 5 c c . . 
+        . . . . b 5 5 5 1 5 5 5 b . . . 
+        . . . . b b c 5 5 5 c b b . . . 
+        . . . . . . c b d b c . . . . . 
+        . . . . . . . b d b . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `)
+    tiles.placeOnTile(star, tiles.getTileLocation(randint(0, 10), randint(0, 80)))
+    star.setVelocity(0, randint(30, 80))
+})
+scene.onOverlapTile(SpriteKind.star6, sprites.dungeon.hazardLava0, function (sprite, location) {
+    star6.destroy()
+})
+sprites.onOverlap(SpriteKind.star4, SpriteKind.Player, function (sprite, otherSprite) {
+    info.changeScoreBy(1)
+    star4.destroy()
+})
+scene.onOverlapTile(SpriteKind.star7, sprites.dungeon.hazardLava0, function (sprite, location) {
+    star7.destroy()
+})
+scene.onOverlapTile(SpriteKind.star3, sprites.dungeon.hazardLava0, function (sprite, location) {
+    star3.destroy()
+})
+let star7: Sprite = null
+let star6: Sprite = null
+let star5: Sprite = null
+let star4: Sprite = null
+let star3: Sprite = null
+let star2: Sprite = null
+let star: Sprite = null
 let mySprite: Sprite = null
+info.setScore(0)
 mySprite = sprites.create(img`
     . . . . . f f 4 4 f f . . . . . 
     . . . . f 5 4 5 5 4 5 f . . . . 
@@ -52,7 +256,7 @@ mySprite = sprites.create(img`
     . . . . . f f b b f f . . . . . 
     `, SpriteKind.Player)
 mySprite.ay = 200
-bird = sprites.create(img`
+star = sprites.create(img`
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
@@ -69,7 +273,115 @@ bird = sprites.create(img`
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
-    `, SpriteKind.bird)
+    `, SpriteKind.star)
+star2 = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.star2)
+star3 = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.star3)
+star4 = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.star4)
+star5 = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.star5)
+star6 = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.star6)
+star7 = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.star7)
 level1()
 forever(function () {
     controller.moveSprite(mySprite, 100, 0)
